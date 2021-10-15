8deaa85075bfdf05f4324bbe17b8e75a38dcdbf2
Guilherme Bublitz <gui.e.bublitz@gmail.com>
2021-10-15 09:21:58 -0300

    Att

--- guilherme.js
+++ guilherme.js
@@ -1238,44 +1238,6 @@ function exercicio13(){
     console.log(listaDeptos);
 }
 
-function exercicio15(){
-    var listaDeptos = [];
-    let codDepto = 0;
-    for(i= 0; i < listaProdutos.length; i++){
-        let produto = listaProdutos[i];
-        if(produto.departamento.idDepto != codDepto){
-            let itemLista = {
-                nomeDepto : produto.departamento.nomeDepto,
-                idDepto : produto.departamento.idDepto,
-                valorInventario: 0
-
-            };
-            listaDeptos.push(itemLista);
-            codDepto = produto.departamento.idDepto;
-        }
-    }
-/*Quanto vale meu estoque por departamento*/ 
-    for(i=0;i< listaProdutos.length; i++){
-        let produto = listaProdutos[i];
-        for(j=0; j < listaDeptos.length; j++){
-            if(produto.departamento.idDepto == listaDeptos[j].idDepto){
-                listaDeptos[j].valorInventario = ((listaDeptos[j].valorInventario) + (produto.preco * produto.qtdEstoque));
-                
-                break;
-            }
-        }
-    }
-/*------------------------*/ 
-    var departamentoMenosValioso = listaDeptos[0];
-    for(i = 0; i < listaDeptos.length; i++){
-        let produto = listaDeptos[i];
-        if (produto.valorInventario < departamentoMenosValioso.valorInventario){
-            departamentoMenosValioso = produto;
-        }
-    }   
-    departamentoMenosValioso.valorInventario = departamentoMenosValioso.valorInventario.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
-    console.log(departamentoMenosValioso);
-}
 
 function all(){
     console.log("");
